9867ff13b937a6fc1c48340c2bd3900847bcd3c1
25cb043-crypto <25cb043@rmkec.ac.in>
2025-09-17 12:56:19 +0530

    version3

---
+++ ram.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    char ch;
+    cout << "enter lower case letter";
+    cin >> ch;
+    char lower=toupper(ch);
+    cout <<"the upper caser is "<<lower<<endl;
+    return 0;
+} 
