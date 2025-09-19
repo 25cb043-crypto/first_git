eb56ebc43141a5a39d6b942ce35cdbfc00c83054
25cb043_crypto <25cb043@rmkec.ac.in>
2025-09-19 14:17:16 +0530

    version 5

---
+++ project2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{    int a;
+    cout << "enter a number" ;
+    cin >> a;
+    if(a!=0 ){
+        cout << "it is a natural number";
+    }else{
+        cout <<"it is not a natural number";
+    } 
+    return 0;
+    
+}
