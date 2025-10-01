12e2147085ae35e7a14fb73b75a80fe6b11308d1
25cb043_crypto <25cb043@rmkec.ac.in>
2025-10-01 07:33:19 +0530

    college

--- ram.cpp
+++ ram.cpp
@@ -1,10 +1,30 @@
 #include <iostream>
 using namespace std;
-int main(){
-    char ch;
-    cout << "enter lower case letter";
-    cin >> ch;
-    char lower=toupper(ch);
-    cout <<"the upper caser is "<<lower<<endl;
-    return 0;
-} 
+int main()
+{
+    string student_name;
+    int roll_number;
+    float marks[5]; // Array for up to 5 subjects
+    int subject;
+    // input of student details:
+    cout << " ENTER STUDENT NAME: ";
+    cin  >> student_name;
+    cout << " ENTER ROLL NUMBER: ";
+    cin >> roll_number;
+    cout << "ENTER NUMBER OF SUBJECTS(3 TO 5) : ";
+    cin >> subject;
+    cout << "ENTER the marks of " << subject << " subject(s): ";
+    for(int i = 0; i < subject; i++)
+    {
+        cin >> marks[i];
+    }
+    // student details:
+    cout << "Student details !" << endl;
+    cout << " NAME : " << student_name << endl;
+    cout << " ROLL NUMBER : " << roll_number << endl;
+    cout << " MARKS : ";
+    for (int i = 0; i < subject; i++) {
+        cout << marks[i] << " ";
+    }
+    cout << endl;
+}
